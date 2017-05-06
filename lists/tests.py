e20427a92aa9ba6969cff1f151d309ecd3d00aa0
geowill <roper.william@gmail.com>
2017-05-06 15:44:34 +0100

    sync tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -1,12 +1,8 @@
-from django.core.urlresolvers import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
-from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-        
