80d16ee4945d31da6b35082193c5a23cfe2ae484
geowill <roper.william@gmail.com>
2017-05-14 16:09:02 +0100

    testsync

--- lists/tests.py
+++ lists/tests.py
@@ -7,12 +7,13 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-   
+
+
 
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
-        self.client.post('/lists/new',data={'item_text': 'A new list item'})
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
